04ae92e0672c62ea018c18095851acd64489d045
Cameron Oehler <coehler@hotmail.com>
2022-11-18 14:53:33 -0600

    Route templates

--- app/index.js
+++ app/index.js
@@ -14,5 +14,8 @@ if(typeof process.env.MONGO_URI === 'undefined') throw "ENV ERROR: Missing MONGO
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to MongoDB'));
 
+app.use('/', require('./routes/main'));
+app.use('/dashboard', require('./routes/dashboard'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ app/routes/main.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+});
+
+router.get('/login', (req, res) => {
+
+});
+
+router.get('/register', (req, res) => {
+
+});
+
+router.get('/about-us', (req, res) => {
+
+});
+
+router.get('/contact', (req, res) => {
+
+});
+
+module.exports = router;
