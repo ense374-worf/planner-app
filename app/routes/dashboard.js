22a404b7656d8c3b94e405394d3bf940c26b7c0f
Cameron Oehler <coehler@hotmail.com>
2022-11-18 15:33:25 -0600

    Basic responses

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
+    res.sendStatus(200);
+});
 
+router.get('/:semId', (req, res) => {
+    res.sendStatus(200);
 });
 
 module.exports = router;
--- app/routes/main.js
+++ app/routes/main.js
@@ -2,23 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    res.render('home');
 });
 
 router.get('/login', (req, res) => {
-
+    res.render('loginRegister', {start: 'login'});
 });
 
 router.get('/register', (req, res) => {
-
+    res.render('loginRegister', {start: 'register'});
 });
 
 router.get('/about-us', (req, res) => {
-
+    res.render('/aboutUs');
 });
 
 router.get('/contact', (req, res) => {
-
+    res.render('contact');
 });
 
 module.exports = router;
