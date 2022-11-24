4817a9b5a61702f0a62fc119ee2ac733fbb0d795
Cameron Oehler <coehler@hotmail.com>
2022-11-24 16:13:24 -0600

    Page Templates

--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -4,11 +4,19 @@ const router = express.Router();
 const {verifyAuthentication} = require('../passport/auth');
 
 router.get('/', verifyAuthentication, (req, res) => {
-    res.sendStatus(200);
+    res.render('dashboard/semesterSelect');
+});
+
+router.get('/new', verifyAuthentication, (req, res) => {
+    res.render('dashboard/newSemester');
 });
 
 router.get('/:semId', verifyAuthentication, (req, res) => {
-    res.sendStatus(200);
+    res.render('dashboard/dashboard');
+});
+
+router.get('/:semId/editClasses', verifyAuthentication, (req, res) => {
+    res.render('dashboard/editClasses');
 });
 
 module.exports = router;
